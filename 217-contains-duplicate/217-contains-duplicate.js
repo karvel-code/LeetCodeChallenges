7960378c790b9bd6867846e63647e18d1776f153
Elvis Kariithi Githinji <velgithinji@gmail.com>
2022-06-15 13:26:18 +0300

    Time: 140 ms (41.80%), Space: 53.8 MB (7.24%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,10 +3,21 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    let uniq_arr = Array.from(new Set(nums))
-     if (uniq_arr.length !== nums.length) {
-         return true
-     } else {
-         return false
-     }
+    // let uniq_arr = Array.from(new Set(nums))
+    //  if (uniq_arr.length !== nums.length) {
+    //      return true
+    //  } else {
+    //      return false
+    //  }
+    
+    let hash = {}
+    for (let i = 0; i < nums.length; i++) {
+        if(!hash[nums[i]]) {
+            hash[nums[i]] = 1
+        } else {
+            hash[nums[i]] += 1
+        }
+    }
+    
+    return Object.keys(hash).length === nums.length ? false : true
 };
