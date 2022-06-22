b73a61b89e5aa3ab1468a5bc0c88b4e210215057
Elvis Kariithi Githinji <velgithinji@gmail.com>
2022-06-22 17:03:35 +0300

    Time: 6211 ms (5.02%), Space: 45.4 MB (36.52%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) {
+        return false
+    }
+    
+    let sArr = [...s]
+    let tArr = [...t]
+    for (let i = 0; i < sArr.length; i++) {
+        if(tArr.indexOf(sArr[i]) === -1) {
+            return false
+        } else {
+            tArr.splice(tArr.indexOf(sArr[i]), 1)
+        }
+    }
+    return true;
+    
+};
