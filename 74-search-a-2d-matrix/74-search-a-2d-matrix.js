06fba40bab25b03b7fca4cb7ab77119b10fed4d2
Elvis Kariithi Githinji <velgithinji@gmail.com>
2022-06-22 11:43:07 +0300

    Time: 98 ms (25.90%), Space: 43.3 MB (10.64%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    for (let i = 0; i < matrix.length; i++) {
+        const set = new Set()
+        for (let j = 0; j < matrix[i].length; j++) {
+            let cell = matrix[i][j]
+            set.add(cell)
+            if (set.has(target)) return true;
+            
+        }
+    }
+    return false
+};
