00cba0caba21257770f44a715c3692ce5f1fe751
Elvis Kariithi Githinji <velgithinji@gmail.com>
2022-06-29 17:15:49 +0300

    Time: 112 ms (16.11%), Space: 42.1 MB (17.38%) - LeetHub

---
+++ 278-first-bad-version/278-first-bad-version.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let start = 0
+        let end  = n
+        while (start < end) {
+            let mid = start + Math.floor((end - start)/2)
+            if (!isBadVersion(mid)) {
+                start = mid + 1
+            } else {
+                end = mid
+            }
+        }
+        return start
+        
+    };
+};
