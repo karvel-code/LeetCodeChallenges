bbe071d6ed1797fa1563d5e9a3c19ac924b12149
Elvis Kariithi Githinji <velgithinji@gmail.com>
2023-02-21 09:04:37 +0300

    Time: 83 ms, Memory: 211.1 MB - LeetHub

---
+++ sqrtx/sqrtx.rb
@@ -0,0 +1,21 @@
+# @param {Integer} x
+# @return {Integer}
+def my_sqrt(x)
+    left = 1
+    right = x
+    
+    return x if x < 2 
+    
+    while left <= right do
+        mid = (left + right) / 2
+        if (mid * mid) == x
+            return mid
+        elsif (mid * mid) > x
+            right = mid -1
+        else (mid * mid) < x
+            left = mid + 1
+        end
+    end
+    right
+    
+end
