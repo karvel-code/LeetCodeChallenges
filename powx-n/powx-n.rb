819e396d9e2202fff067876dd2a305176af6615e
Elvis Kariithi Githinji <velgithinji@gmail.com>
2023-03-23 07:36:22 +0300

    Time: 94 ms, Memory: 211.4 MB - LeetHub

--- powx-n/powx-n.rb
+++ powx-n/powx-n.rb
@@ -2,5 +2,17 @@
 # @param {Integer} n
 # @return {Float}
 def my_pow(x, n)
-    return x**n
+    result = 1
+    k = n.abs
+    
+    return 1 if n == 0
+    
+    while k >= 1 
+        if k % 2 == 1
+            result *= x
+        end
+            x = x * x
+            k = k/2
+    end
+    return n > 0 ? result : 1.0/result
 end
