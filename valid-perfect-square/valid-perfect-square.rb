b49be78079e6ed7f550df80519d5b9da17ddd48b
Elvis Kariithi Githinji <velgithinji@gmail.com>
2023-03-24 07:49:46 +0300

    Time: 84 ms, Memory: 211.1 MB - LeetHub

--- valid-perfect-square/valid-perfect-square.rb
+++ valid-perfect-square/valid-perfect-square.rb
@@ -6,12 +6,12 @@ def is_perfect_square(num)
     
     return true if num == 1
     
-    while left < right
+    while left <= right
         mid = (left + right) / 2
         if (mid * mid) == num
             return true
         elsif (mid * mid) > num
-            right = mid
+            right = mid - 1
         else
             left = mid + 1
         end
